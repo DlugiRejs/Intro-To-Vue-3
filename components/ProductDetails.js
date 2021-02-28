10bc8445b02d6992016a2d5b03881333cb84eda2
DlugiRejs <dlugi.rejs@gmail.com>
2021-02-28 13:25:22 +0100

    Lesson 9 end

---
+++ components/ProductDetails.js
@@ -0,0 +1,11 @@
+app.component("product-details", {
+    props: {
+        details: {
+            type: Array,
+            required: true,
+        }
+    },
+    template:
+        /*html*/
+        `<div> <ul><li v-for="detail in details">{{ detail }}</li></ul> </div>`
+})
